3f20326915518773cec43af83b2347e15ef43784
Toacin <toacinp@outlook.com>
2022-10-17 22:35:51 -0700

    add employee and update employee role now successfully functions in application

--- index.js
+++ index.js
@@ -50,6 +50,44 @@ let whatRole = [
     }
 ]
 
+let whatEmployee = [
+    {
+        name: "firstName",
+        message: "What is the first name of the employee you would like to add?"
+    },
+    {
+        name: "lastName",
+        message: "What is the last name of the employee you would like to add?"
+    },
+    {
+        name: "role",
+        type: "list",
+        message: "What is the role of the current employee?",
+        choices: ["Sales Lead", "Salesperson", "Lead Engineer", "Software Engineer", "Account Manager", "Accountant", "Legal Team Lead", "Attorney"]
+    },
+    {
+        type: "list",
+        name: "whichManager",
+        message: "Who is the manager of the current employee?",
+        choices: ["Jane", "James", "Miguel", "Morgan", "Derrick", "Toacin", "Disney", "Ian", "None"]
+    }
+]
+
+let updateEmployeeQuestion = [
+    {
+        type: "list",
+        name: "employeeName",
+        message: "What is the name of the employee who's role you'd like to update?",
+        choices: ["Jane", "James", "Miguel", "Morgan", "Derrick", "Toacin", "Disney", "Ian", "None"]
+    },
+    {
+        type: "list",
+        name: "newRole",
+        message: "What is the new role of this employee?",
+        choices: ["Sales Lead", "Salesperson", "Lead Engineer", "Software Engineer", "Account Manager", "Accountant", "Legal Team Lead", "Attorney"]
+    }
+]
+
 function askInit () {
     inquirer.prompt(initialQuestion).then((answers) => {
         switch (answers.whatToDo) {
@@ -69,7 +107,10 @@ function askInit () {
                 addRole();
                 break;
             case "Add Employee":
-                addRole();
+                addEmployee();
+                break;
+            case "Update Employee Role":
+                updateEmployee();
                 break;
             case "Quit":
                 console.log("Good Bye!");
@@ -106,7 +147,9 @@ let addDepartment = () => {
     inquirer.prompt(whatDepartment)
     .then((response) => {
         db.query(`INSERT INTO department (name) VALUES (?);`, [response.departmentName], (err, data) => {
+            console.log("\n-----------------------------------------\n")
             console.log("New department has been successfully added!")
+            console.log("\n-----------------------------------------\n")
             askInit();
         })
     })
@@ -128,8 +171,138 @@ let addRole = () => {
             default:
                 break;
         }
-        db.query(`INSERT INTO role (title, salary, department_id) VALUES (?,?,?);`, [response.roleName, response.salary, departID], (err, data) => {
+        db.query(`INSERT INTO role (title, salary, department_id) VALUES (?,?,?);`, 
+        [response.roleName, response.salary, departID], 
+        (err, data) => {
+            console.log("\n-----------------------------------------\n")
             console.log("New role has been successfully added!")
+            console.log("\n-----------------------------------------\n")
+            askInit();
+        })
+    })
+};
+
+let addEmployee = () => {
+    inquirer.prompt(whatEmployee)
+    .then((response) => {
+        let roleID;
+        let managerID;
+        switch (response.role) {
+            case "Sales Lead": roleID = 1;
+                break;
+            case "Salesperson": roleID = 2;
+                break;
+            case "Lead Engineer": roleID = 3;
+                break;
+            case "Software Engineer": roleID = 4;
+                break;
+            case "Account Manager": roleID = 5;
+                break;
+            case "Accountant": roleID = 6;
+                break;
+            case "Legal Team Lead": roleID = 7;
+                break;
+            case "Attorney": roleID = 8;
+                break;
+            default:
+                break;
+        }
+        switch (response.whichManager) {
+            case "Jane": managerID = 1;
+                break;
+            case "James": managerID = 2;
+                break;
+            case "Miguel": managerID = 3;
+                break;
+            case "Morgan": managerID = 4;
+                break;
+            case "Derrick": managerID = 5;
+                break;
+            case "Toacin": managerID = 6;
+                break;
+            case "Disney": managerID = 7;
+                break;
+            case "Ian": managerID = 8;
+                break;
+            case "None":
+                db.query(`INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?,?,?, NULL);`, 
+                [response.firstName, response.lastName, roleID], 
+                (err, data) => {
+                    if (err) throw err;
+                    console.log("\n-----------------------------------------\n")
+                    console.log("New employee has been successfully added!")
+                    console.log("\n-----------------------------------------\n")
+                    askInit();
+                })
+                askInit();
+                return;
+            default:
+                break;
+        }
+        db.query(`INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?,?,?,?);`, 
+        [response.firstName, response.lastName, roleID, managerID], 
+        (err, data) => {
+            if (err) throw err;
+            console.log("\n-----------------------------------------\n")
+            console.log("New employee has been successfully added!")
+            console.log("\n-----------------------------------------\n")
+            askInit();
+        })
+    })
+};
+
+let updateEmployee = () => {
+    inquirer.prompt(updateEmployeeQuestion)
+    .then((response) => {
+        let roleID;
+        let employeeID;
+        switch (response.newRole) {
+            case "Sales Lead": roleID = 1;
+                break;
+            case "Salesperson": roleID = 2;
+                break;
+            case "Lead Engineer": roleID = 3;
+                break;
+            case "Software Engineer": roleID = 4;
+                break;
+            case "Account Manager": roleID = 5;
+                break;
+            case "Accountant": roleID = 6;
+                break;
+            case "Legal Team Lead": roleID = 7;
+                break;
+            case "Attorney": roleID = 8;
+                break;
+            default:
+                break;
+        }
+        switch (response.employeeName) {
+            case "Jane": employeeID = 1;
+                break;
+            case "James": employeeID = 2;
+                break;
+            case "Miguel": employeeID = 3;
+                break;
+            case "Morgan": employeeID = 4;
+                break;
+            case "Derrick": employeeID = 5;
+                break;
+            case "Toacin": employeeID = 6;
+                break;
+            case "Disney": employeeID = 7;
+                break;
+            case "Ian": employeeID = 8;
+                break;
+            default:
+                break;
+        }
+        db.query(`UPDATE employee SET role_id = ? WHERE id = ?;`, 
+        [roleID, employeeID], 
+        (err, data) => {
+            if (err) throw err;
+            console.log("\n-----------------------------------------\n")
+            console.log("New employee has been successfully added!")
+            console.log("\n-----------------------------------------\n")
             askInit();
         })
     })
